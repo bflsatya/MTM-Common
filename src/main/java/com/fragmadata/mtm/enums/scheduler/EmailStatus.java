da4a70658565099d932f7ddc636f2bb4a8830ab1
Satya Dev Areti <satyad@fragmadata.com>
2025-02-10 20:43:22 +0530

    Moved ImportStatus Enum to EmailStatus Enum

--- src/main/java/com/fragmadata/mtm/enums/scheduler/EmailStatus.java
+++ src/main/java/com/fragmadata/mtm/enums/scheduler/EmailStatus.java
@@ -1,5 +1,8 @@
 package com.fragmadata.mtm.enums.scheduler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum EmailStatus {
 
     SUCCESS,
@@ -20,5 +23,32 @@ public enum EmailStatus {
     EMAIL_CONFIG_NOT_MATCHED,
     QUEUED_FOR_ATTACHMENT_IDENTIFICATION,
     PROCESSED,
-    FAILED
+    FAILED;
+
+    EmailStatus() {}
+
+
+    private static List<String> successStatusList = new ArrayList<>();
+    private static List<String> errorStatusList = new ArrayList<>();
+
+    static {
+        successStatusList.add(SUCCESS.name());
+        successStatusList.add(PROCESSED.name());
+
+        errorStatusList.add(FAILED.name());
+        errorStatusList.add(FAILURE.name());
+        errorStatusList.add(CONFIG_NOT_MATCHED.name());
+        errorStatusList.add(EMAIL_CONFIG_NOT_MATCHED.name());
+        errorStatusList.add(SHEET_CONFIG_MISMATCHED.name());
+        errorStatusList.add(PARSING_FAILED.name());
+    }
+
+
+    public static List<String> getSuccessStatusList() {
+        return successStatusList;
+    }
+
+    public static List<String> getErrorStatusList() {
+        return errorStatusList;
+    }
 }
